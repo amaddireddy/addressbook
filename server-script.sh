2328cccf86bfbe4e62b32124e2b112374dcfff4b
amaddireddy <98331624+amaddireddy@users.noreply.github.com>
2022-02-20 12:43:52 +0530

    Update server-script.sh

--- server-script.sh
+++ server-script.sh
@@ -2,15 +2,15 @@
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y 
-if [ -d "address" ] 
+if [ -d "addressbook" ] 
 then
   echo "repo is cloned and exists"
-  cd /home/ec2-user/address
+  cd /home/ec2-user/addressbook
   git pull origin democicd
 else
-  git clone https://github.com/amaddireddy/address.git
+  git clone https://github.com/amaddireddy/addressbook.git
 fi
-cd /home/ec2-user/address
+cd /home/ec2-user/addressbook
 mvn package  
 sudo yum install docker -y
 sudo systemctl start docker
